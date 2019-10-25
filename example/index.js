0eacf8c393332a6aaa5c9c25da8eeae7786c4c8e
Brian Reavis <brian@thirdroute.com>
2019-10-25 12:22:18 -0700

    Updated example

--- example/index.js
+++ example/index.js
@@ -6,27 +6,22 @@ var strata = tilestrata();
 strata
 	.layer('a')
 		.route('tile.png')
-			.use(disk({dir: './.tilecache/a'}))
+			.use(disk.cache({ dir: './.tilecache/a' }))
 			.use(mapnik({
-				xml: '/path/to/a.xml',
+				pathname: '/path/to/a.xml',
 				scale: 1,
 				tileSize: 256
-			})
-	.layer('a')
+			}))
+	.layer('b')
 		.route('tile.png')
-			.use(disk({dir: './.tilecache/b'}))
+			.use(disk.cache({ dir: './.tilecache/b' }))
 			.use(mapnik({
-				xml: '/path/to/b.xml',
+				pathname: '/path/to/b.xml',
 				scale: 1,
 				tileSize: 256
-			});
+			}));
 
-strata.listen(8080, function() {
-	console.log('Listening on 8080...');
-});
-
-process.on('SIGTERM', function() {
-	strata.close(function() {
-		process.exit(0);
-	});
+strata.listen(8080, function(err) {
+	if (err) throw err;
+	console.log('Listening on port 8080...');
 });
